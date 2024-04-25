68bfc9c6aacf4dc8b5b6fefa389f231a72c39f45
ShibinRasheed <shibinrasheed333@gmail.com>
2024-04-25 15:43:22 +0530

    Navbar Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import styles from './style'
+import {Navbar} from "./components"
 
 function App() {
   return (
     <>
-      <div className='bg-primary text-white w-full overflow-hidden'>
+      <div className='bg-primary w-full overflow-hidden'>
           <div className={`${styles.paddingX} ${styles.flexCenter}`}>
             <div className={`${styles.boxWidth}`}>  
-              Navbar
+              <Navbar/>
             </div>
           </div>
 
