1041accb040c70ee76d72e5861f2efbf2e676cd7
ShibinRasheed <shibinrasheed333@gmail.com>
2024-04-25 14:49:13 +0530

    Basic Structure planed out

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-import Navbar from './components/Navbar'
-import "./index.css"
+import styles from './style'
 
 function App() {
   return (
     <>
-    <Navbar />
+      <div className='bg-primary text-white w-full overflow-hidden'>
+          <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>  
+              Navbar
+            </div>
+          </div>
+
+          <div className={`bg-primary ${styles.flexStart}`}>
+            <div className={`${styles.boxWidth}`}>
+              Hero
+            </div>
+          </div>
+
+          <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+            <div className={`${styles.boxWidth}`}>
+              Stats
+              Business
+              Billing 
+              CardDeal 
+              Testimonials 
+              Clients 
+              CTA 
+              Footer
+            </div>
+          </div>
+
+      </div>
     </>
   )
 }
